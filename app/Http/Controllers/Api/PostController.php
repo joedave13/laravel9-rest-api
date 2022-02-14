a88dd6d3373c5ec85c131b53c568b5c623f63b0d
Joshua Davian <joshuadavian@gmail.com>
2022-02-14 09:39:23 +0700

    add post data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -39,7 +40,26 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('post', $image->hashName(), 'public');
+
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        return new PostResource(true, 'Post added successfully!', $post);
     }
 
     /**
